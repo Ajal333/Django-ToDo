e2faccb5f4ecf81be431d252c1e900a0771b736a
Ajal333 <ajapoos1999@gmail.com>
2020-03-24 23:53:34 +0530

    Enthokkeyo cheyth

--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from todo_app.models import Category, todo
 from todo_app.forms import CategoryForm, todoForm
+from datetime import date, datetime
 
 # Create your views here.
 
@@ -24,6 +25,7 @@ from todo_app.forms import CategoryForm, todoForm
 
 def add_category(request) :
     form1 = CategoryForm()
+    category = Category.objects.all()
     if request.method == 'POST' :
         form = CategoryForm(request.POST)
 
@@ -32,9 +34,10 @@ def add_category(request) :
 
             return render(request,'todo/add_category.html',{'message':'Category Added','form':form1})
         
-    return render(request,'todo/add_category.html',{'form':form1})
+    return render(request,'todo/add_category.html',{'form':form1,'category':category})
         
 def add_list(request) :
+    due_date()
     category = Category.objects.all()
     todo_1 = todo.objects.all()
     form1 = todoForm() 
@@ -80,4 +83,30 @@ def del_list(request, list1) :
             print('{} is deleted'.format(list1))
         else :
             continue
-    return render(request,'todo/add_list.html',{'form':form1,'category':category,'todo':todo_1})
+    todo_2 = todo.objects.all()
+    return render(request,'todo/add_list.html',{'form':form1,'category':category,'todo':todo_2})
+
+def del_cat(request,cat) :
+    form1 = todoForm()
+    category = Category.objects.all()
+    todo_1 = todo.objects.all()
+    for cats in category :
+        if cats.category == cat :
+            Category.objects.filter(category = cat).delete()
+            print('{} is deleted'.format(cat))
+        else :
+            continue
+    todo_2 = todo.objects.all()
+    category_2 = Category.objects.all()
+    return render(request,'todo/add_list.html',{'form':form1,'category':category_2,'todo':todo_2})
+
+def due_date() :    
+    category = Category.objects.all()
+    list1 = todo.objects.all()
+    for data in list1 :
+        year1 = datetime.now()
+        year4 = data.due_date
+        year2 = data.due_date.strftime('%Y/%m/%d')
+        year3 = str(year1.year)+'/'+str(year1.month)+'/'+str(year1.day)
+        if year4 == year3 :
+            print('ammachi')
